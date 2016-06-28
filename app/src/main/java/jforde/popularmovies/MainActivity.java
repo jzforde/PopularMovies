9ced43dbfb7d395332c93b2946230df4adb55ec2
Jillian Forde <jillianforde@Jillians-MBP.home>
2016-06-28 14:14:06 -0400

    remove unused code and back navigation

--- app/src/main/java/jforde/popularmovies/MainActivity.java
+++ app/src/main/java/jforde/popularmovies/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,20 +15,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jforde.popularmovies.view.R;
-
-/**
- * Created by jillianforde on 6/19/16.
- */
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private DrawerLayout mDrawerLayout;
-    private MovieSorter movies = new MovieSorter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +31,12 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         final ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayHomeAsUpEnabled(false);
 
         SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), MainActivity.this);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         if (viewPager != null) {
-            //setUpViewPager(viewPager);
             viewPager.setAdapter(mSectionsPagerAdapter);
 
         }
@@ -110,42 +99,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void setUpViewPager(ViewPager viewPager){
-        Adapter adapter = new Adapter(getSupportFragmentManager());
-        adapter.addFragment(new PopularMoviesFragment(), "Most Popular");
-        adapter.addFragment(new TopMoviesFragment(), "Top Rated Movies");
-        viewPager.setAdapter(adapter);
-    }
-
-    static class Adapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragments = new ArrayList<>();
-        private final List<String> mFragmentTitles = new ArrayList<>();
-
-        public Adapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragments.add(fragment);
-            mFragmentTitles.add(title);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragments.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragments.size();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitles.get(position);
-        }
     }
 
 }
