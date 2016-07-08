f1ce7fa916f09b01dd7c33f1ece89d88aa34ef93
Jillian Forde <jillianforde@Jillians-MBP.home>
2016-07-08 09:56:13 -0400

    add floating action button to mark as favorite and snackbar to acknowledge button click

--- app/src/main/java/jforde/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/jforde/popularmovies/MovieDetailsActivity.java
@@ -1,19 +1,24 @@
 package jforde.popularmovies;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 import jforde.popularmovies.view.R;
-
 
 /**
  * Created by jillianforde on 6/18/16.
@@ -22,16 +27,19 @@ import jforde.popularmovies.view.R;
 public class MovieDetailsActivity extends AppCompatActivity{
     public static final String EXTRA_NAME = "movie_name";
     public static final String EXTRA_POSTER_NAME = "poster_img";
+    public static final String EXTRA_BACKDROP_NAME = "backdrop_path";
     public static final String EXTRA_OVERVIEW = "movie_overview";
     public static final String EXTRA_VOTE_AVG = "vote_avg";
     public static String EXTRA_RELEASE_DATE = "release_date";
     private final String TAG = "MovieDetailsActivity";
     String moviePosterImg;
+    private CoordinatorLayout mCoordinatorLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_detail);
-
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.movie_details);
         Intent intent = getIntent();
         TextView overview = (TextView) findViewById(R.id.overview);
         TextView releaseDate = (TextView) findViewById(R.id.release_date);
@@ -53,8 +61,17 @@ public class MovieDetailsActivity extends AppCompatActivity{
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle(movieTitle);
         loadBackdrop();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.mark_as_fav_btn);
+        Drawable noFillHeart = getResources().getDrawable(R.drawable.ic_favorite_border_black_36dp);
+        int heartColor = getResources().getColor(R.color.white);
+        noFillHeart.setColorFilter(heartColor, PorterDuff.Mode.SRC_IN);
+
     }
 
+    private void loadPoster(){
+        //final ImageView poster = (ImageView) findViewById(R.id.details_poster);
+    }
     private void loadBackdrop() {
         final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
         Glide.with(this).load(moviePosterImg).centerCrop().into(imageView);
@@ -66,4 +83,13 @@ public class MovieDetailsActivity extends AppCompatActivity{
         getMenuInflater().inflate(R.menu.sample_actions, menu);
         return true;
     }
+
+    public void markAsFavorite(View view){
+        Snackbar successfullyMarkedAsFav = Snackbar.make
+                (mCoordinatorLayout, "Added to favorites", Snackbar.LENGTH_LONG);
+        successfullyMarkedAsFav.show();
+    }
+
+
+
 }
---
+++ app/src/main/java/jforde/popularmovies/MovieTrailerRequest.java
@@ -0,0 +1,8 @@
+package jforde.popularmovies;
+
+/**
+ * Created by jillianforde on 6/30/16.
+ */
+
+public class MovieTrailerRequest {
+}
